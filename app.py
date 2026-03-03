2b76fd9ab173dfca4ac7671a48a94d92ebc6e5b6
KDH2929 <165474536+KDH2929@users.noreply.github.com>
2026-03-03 12:36:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,6 @@ from pymongo import MongoClient
 client = MongoClient('mongodb://test:test@localhost', 27017)
 db = client.dbjungle
 
-SECRET_KEY = 'YOUR_SECRET_KEY'
-
 ## HTML을 주는 부분
 @app.route('/')
 def home():
